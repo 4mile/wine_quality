848f1a95e0423686eb126bb3762bd4ee58149d93
Ian Langworth ☠ <ian@langworth.com>
2018-06-18 09:47:35 -0700

    Add Dockerfile and kernelgateway config

---
+++ jupyter_kernel_gateway_config.py
@@ -0,0 +1,4 @@
+c.KernelGatewayApp.api = 'kernel_gateway.notebook_http'
+c.KernelGatewayApp.ip = '*'
+#c.KernelGatewayApp.seed_uri = '/notebooks/wine_quality_prediction_new_data.ipynb'
+c.KernelGatewayApp.seed_uri = '/notebooks/test.ipynb'
